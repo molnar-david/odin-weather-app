11fce072295d3b10a4d0154eaea685a6220e5efa
molnar-david <d.molnarka@hotmail.com>
2024-09-28 11:45:16 +0200

    Implement JS to display weather data (except icons)

--- script.js
+++ script.js
@@ -17,6 +17,42 @@ function processWeatherData(weatherData) {
     return result;
 }
 
+function displayWeatherData(data) {
+    document.getElementById('location').textContent = data.location;
+    document.getElementById('description').textContent = data.description;
+    document.getElementById('current-time').textContent = data.time.slice(0, 5);
+    document.getElementById('current-conditions').textContent = data.conditions;
+    document.getElementById('current-temp').textContent = data.temp;
+    document.getElementById('sunrise').textContent = data.sunrise.slice(0, 5);
+    document.getElementById('sunset').textContent = data.sunset.slice(0, 5);
+
+    const forecastTimes = Array.from(document.getElementsByClassName('forecast-time'));
+    // Don't forget icons...
+    const forecastTemps = Array.from(document.getElementsByClassName('forecast-temp'));
+
+    // Target times: 00:00 - 21:00 in 3-hour increments,
+    // starting with the one directly after the current time
+    let targetHour = (Math.floor(Number(data.time.slice(0, 2)) / 3) + 1) * 3;
+    for (let i = 0; i < forecastTimes.length; i++) {
+        // Make sure we don't go above 23:00
+        targetHour %= 24;
+        const targetForecast = data.forecast[0].hours[targetHour];
+        forecastTimes[i].textContent = targetForecast.datetime.slice(0, 5);
+        forecastTemps[i].textContent = targetForecast.temp;
+        targetHour += 3;
+    }
+
+    const alerts = document.getElementById('alerts-container');
+    alerts.textContent = '';
+    data.alerts.forEach((alert) => {
+        const alertDiv = document.createElement('div');
+        // Capitalize headline
+        alertDiv.textContent += alert.headline[0].toUpperCase() + alert.headline.slice(1) + ': ';
+        alertDiv.textContent += alert.description;
+        alerts.appendChild(alertDiv);
+    });
+}
+
 async function getWeatherData(loc) {
     try {
         const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
@@ -28,6 +64,7 @@ async function getWeatherData(loc) {
         }
         const weatherData = await response.json();
 
+        displayWeatherData(processWeatherData(weatherData));
         return processWeatherData(weatherData);
     } catch (err) {
         console.log(err);
